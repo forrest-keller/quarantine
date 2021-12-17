d39e91d6fd71ce697fe432bc032980bb4a16481b
Forrest Keller <forrestblackburnkeller@gmail.com>
2021-12-17 13:16:41 +0700

    Create first poem

---
+++ src/components/poem.tsx
@@ -0,0 +1,71 @@
+import {
+  Box,
+  Center,
+  Heading,
+  HeadingProps,
+  Spacer,
+  Stack,
+  StackProps,
+  Text,
+  TextProps,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import Image, { ImageProps } from "next/image";
+import { CSSProperties, FunctionComponent } from "react";
+
+export interface PoemProps {
+  title: string;
+  number: number;
+  imageSrc: ImageProps["src"];
+  imageAlt: string;
+}
+
+export const Poem: FunctionComponent<PoemProps> = ({
+  title,
+  number,
+  imageAlt,
+  imageSrc,
+  children,
+}) => {
+  const headingStyleProps = useBreakpointValue<CSSProperties>({
+    lg: {
+      writingMode: "sideways-lr",
+    },
+  });
+  const headingProps: HeadingProps = {
+    style: headingStyleProps,
+  };
+  const textProps: TextProps = {
+    whiteSpace: "pre-line",
+    lineHeight: "2em",
+  };
+  const imageProps: Partial<ImageProps> = {
+    width: 500,
+    height: 500,
+  };
+  const stackProps: StackProps = {
+    direction: { base: "column", lg: "row" },
+    gap: "1em",
+  };
+  const headingStackProps: StackProps = {
+    direction: { lg: "column-reverse" },
+    width: { base: 500, lg: "3em" },
+    alignSelf: { base: "center", lg: "initial" },
+  };
+
+  return (
+    <Stack {...stackProps}>
+      <Stack {...headingStackProps}>
+        <Heading {...headingProps}>{number}</Heading>
+        <Spacer />
+        <Heading {...headingProps}>{title}</Heading>
+      </Stack>
+      <Center>
+        <Image src={imageSrc} alt={imageAlt} {...imageProps} />
+      </Center>
+      <Center>
+        <Text {...textProps}>{children}</Text>
+      </Center>
+    </Stack>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,51 @@
+import {
+  Center,
+  Container,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
+import documentsImage from "../../public/images/documents.jpg";
+import { Poem } from "../components/poem";
+
+const docmentsImageAlt = "Image of documents on a wooden floor";
+const documentsTitle = "Documents";
+const documentsText = `How can they reduce a person to numbers?
+Cold hands from above guide both reflections on the glass pane.
+Does either party know the significance of what they hold?
+Beneath the pulp, ink loses figure, diminished to reality.
+The stamp marks: Approved.`;
 
 const Home: NextPage = () => {
-  return <p>Home</p>;
+  return (
+    <Container maxWidth="7xl">
+      <Center marginY="5em">
+        <Heading as="h1" fontSize="5xl" color="red.700">
+          Quarantine
+        </Heading>
+      </Center>
+      <Stack gap="1em">
+        <Poem
+          title={documentsTitle}
+          number={1}
+          imageSrc={documentsImage}
+          imageAlt={docmentsImageAlt}
+        >
+          {documentsText}
+        </Poem>
+      </Stack>
+      <Center marginY="5em">
+        <Text color="gray.400">
+          Made by&nbsp;
+          <Link href="https://forrestkeller.com" isExternal>
+            Forrest Keller
+          </Link>
+        </Text>
+      </Center>
+    </Container>
+  );
 };
 
 export default Home;
